2c4c03a36564fd1c9840a5b4d561ff235eaeab71
Harmon Developer <developer@harmon.ml>
2023-01-06 16:45:35 +0300

    Main

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    token: "MTA1NjU4OTY2MTIxODI4MzYwMQ.GrCxRS.Xy9gW5tVToInmhztmylsPDMkUkm0oyHhFpb8-MTA1NjU4OTY2MTIxODI4MzYwMQ.GpIRdP.ZGTMclWPtVcei32yrV8j76J2_GGHBRzZsEXOAk", 
+    prefix: "-"
+}
+
